05f90e80df8fa036d68a33dd143ae49df8846bc6
nahuel.castillo44@gmail,com <nahuel.castillo44@gmail.com>
2023-09-05 14:32:02 -0300

    Version actualizada

--- js/addproyect.js
+++ js/addproyect.js
@@ -74,22 +74,27 @@ window.onload = function(){
         if(bttc){
         bttc.addEventListener("click", function(){
         div.innerHTML = `
-        <form action="" class="formm">
-        <p>Nombre:</p>
-        <input type="text" class="contf">
-
-        <p>Correo electronico:</p>
-        <input type="mail" class="contf">
-
-        <p>Asunto:</p>
-        <input type="text" class="contf">
-
-        <p>Mensaje:</p>
-        <input type="text" class="contf">
-
-        <button type="button">Send</button>
-
+        <div class="container">
+        <h1>Contact Me</h1>
+        <form target="_blank" action="https://formsubmit.co/nahuel.castillo44@gmail.com" method="POST">
+        <div class="form-group">
+        <div class="form-row">
+        <div class="col">
+          <input type="text" name="name" class="form-control" placeholder="Full Name" required>
+        </div>
+        <div class="col">
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+        </div>
+        </div>
+        </div>
+        <div class="form-group">
+        <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+        </div>
+        <div id="btt">
+        <button type="submit" class="btn btn-lg btn-dark btn-block"> <span>Submit Form  </span> </button>
+        </div>
         </form>
+        </div>
         ` 
     })}
 
